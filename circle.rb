ced8bfb1bcb882eacbaa6a066865269598427dfc
Sprite Guard <parker.tristan.james@gmail.com>
2020-03-23 20:40:48 -0700

    circle
    Generates points for polygons and circles with either a fixed number of
    sides or scaling the number of sides based on radius.

---
+++ circle.rb
@@ -0,0 +1,42 @@
+class Circle
+  attr_accessor :resolution, :scaling, :scale_rate, :color
+
+  def initialize resolution: 8, scaling: :dynamic, scale_rate: 8, color: [100, 0, 100, 255]
+    @resolution = resolution
+    @scaling = scaling
+    @scale_rate = scale_rate
+    @color = color
+  end
+
+  def draw circle, renderer
+    all_points = divide circle
+    all_points.each_cons(2) do |a|
+      renderer << [a[0], a[1], color]
+    end
+    renderer << [all_points[0], all_points[-1], color]
+  end
+
+  def divide c
+    if scaling == :dynamic
+      res = @resolution + (c[:r] / scale_rate).floor
+    else
+      res = @resolution
+    end
+
+    TAU = 6.28318530717958647692
+    angle = TAU / res
+
+    points = []
+
+    res.times do |n|
+      x_offset = c[:r] * Math.sin(n * angle)
+      y_offset = c[:r] * Math.cos(n * angle)
+      x = c[:x] + x_offset
+      y = c[:y] + y_offset
+      points << [x,y]
+    end
+    return points
+  end
+
+end
+
